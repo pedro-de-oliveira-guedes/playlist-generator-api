a6cd2e8db1607ca7232f3e0763685b3321968fd8
pedro-de-oliveira-guedes <pedro.og2002@gmail.com>
2025-01-16 23:01:21 -0300

    fix: changing cluster IP to the correct one

--- client/cli_requests.sh
+++ client/cli_requests.sh
@@ -2,6 +2,6 @@ wget --server-response \
     --output-document response.json \
     --header='Content-Type: application/json' \
     --post-data '{"songs": ["Panda"]}' \
-    http://10.42.0.114:52055/api/recommend
+    http://10.43.113.159:52055/api/recommend
 
 cat response.json
